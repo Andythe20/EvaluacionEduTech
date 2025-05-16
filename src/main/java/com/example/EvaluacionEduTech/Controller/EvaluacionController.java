116ee49d8de0510220a71624bae058a633375f26
Nicolas-15 <nic.lopezp@duocuc.cl>
2025-05-16 13:10:14 -0400

    Add files via upload

--- src/main/java/com/example/EvaluacionEduTech/Controller/EvaluacionController.java
+++ src/main/java/com/example/EvaluacionEduTech/Controller/EvaluacionController.java
@@ -3,7 +3,9 @@ package com.example.EvaluacionEduTech.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.EvaluacionEduTech.Model.Evaluacion;
 import com.example.EvaluacionEduTech.Service.EvaluacionService;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
+
 
 @RestController
 @RequestMapping("/api/v1/evaluaciones")
@@ -19,14 +25,33 @@ public class EvaluacionController {
     @Autowired
     private EvaluacionService service;
 
+    //Obtener Evaluaciones
     @GetMapping("/")
     public List<Evaluacion> listarEvaluaciones(){
         return service.listarEvaluaciones();
     }
 
+    //Agregar Evaluaciones
     @PostMapping("/")
     public Evaluacion guardarEvaluacion(@RequestBody Evaluacion evaluacion){
         return service.guardarEvaluacion(evaluacion);
     }
 
+    //Obtener Evaluacion por id
+    @GetMapping("/{evaluacionId}")
+    public Evaluacion BuscarPorId(@PathVariable Long evaluacionId) {
+        return service.getEvaluacionId(evaluacionId);
+    }
+
+    //Actualizar Evaluacion
+    @PutMapping("/{evaluacionId}")
+    public Evaluacion ActualizaEvaluacion(@RequestBody Evaluacion evaluacion,@PathVariable Long evaluacionId) {        
+        return service.saveEvaluacion(evaluacion);
+    }
+    
+    //Eliminar Evaluacion
+    @DeleteMapping("/{evaluacionId}")
+    public void EliminarEvaluacion(@PathVariable Long evaluacionId){
+        service.deleteEvaluacion(evaluacionId);
+    }
 }
--- src/main/java/com/example/EvaluacionEduTech/Repository/EvaluacionRepository.java
+++ src/main/java/com/example/EvaluacionEduTech/Repository/EvaluacionRepository.java
@@ -2,11 +2,9 @@ package com.example.EvaluacionEduTech.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import com.example.EvaluacionEduTech.Model.Evaluacion;
 
 @Repository
 public interface EvaluacionRepository extends JpaRepository<Evaluacion, Long>{
-
     
 }
